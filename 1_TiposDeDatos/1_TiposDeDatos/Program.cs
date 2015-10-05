e7d8d34386d2dd6b6cc038f8c3b366d7b8b82c2a
senquevila <senquevila@gmail.com>
2015-10-04 23:59:27 -0600

    Primer commit. Agregados tipos de datos y funciones

---
+++ 1_TiposDeDatos/1_TiposDeDatos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_TiposDeDatos
+{
+    class Program
+    {
+        enum Dias {Domingo, Lunes, Martes, Miercoles, Jueves, Viernes, Sabado};
+
+        static void Main(string[] args)
+        {
+            // Tipos de datos unsigned
+            Console.WriteLine("Entero sin signo minimo: " + uint.MinValue + ", maximo: " + uint.MaxValue);
+
+            // Tener cuidado de utilizar tipos unsigned
+            uint NoNegativo = 0;
+            NoNegativo--;
+
+            Console.WriteLine("Valor anterior al cero (supuestamente): " + NoNegativo);
+
+            // Diferentes tamaños para tipos de datos
+            Console.WriteLine("Entero 16 bits minimo: " + Int16.MinValue + ", maximo: " + Int16.MaxValue);
+            Console.WriteLine("Entero 32 bits minimo: " + Int32.MinValue + ", maximo: " + Int32.MaxValue);
+            Console.WriteLine("Entero 64 bits minimo: " + Int64.MinValue + ", maximo: " + Int64.MaxValue);
+
+            // Nuevos tipos: decimal
+            decimal dec = 3.52343m;    // para diferenciar de un double si agrega el sufijo m
+            Console.WriteLine("El valor es: {0:C}", dec);
+
+            // Tipos objeto: 
+            
+            // Fecha
+            DateTime fecha = new DateTime(2015, 1, 1);
+            Console.WriteLine(fecha);          
+
+            // Uso de un enum
+            Console.WriteLine(Dias.Domingo);           
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ 2_Funciones/2_Funciones/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Multiples parametros
+            Console.WriteLine("1 + 2 = {0}", Suma(1, 2));
+            Console.WriteLine("1 + 2 + 3 = {0}", Suma(1, 2, 3));
+
+            int valor = 10;
+            // Paso por valor (el comun en JAVA)
+            Console.WriteLine("Paso por valor antes = {0}", valor);
+            Cuadrado_Valor(valor);
+            Console.WriteLine("Paso por valor despues = {0}", valor);
+
+            // Paso por refencia (Agregado en C#)
+            Console.WriteLine("Paso por referencia antes = {0}", valor);
+            Cuadrado_Referencia(ref valor);
+            Console.WriteLine("Paso por referencia despues = {0}", valor);
+
+            Console.ReadKey(true);
+        }
+        
+        static int Suma(params int[] parametros)
+        {
+            int total;
+
+            total = 0;
+
+            foreach (int numero in parametros) {
+                total += numero;
+            }
+
+            return total;
+        }
+
+        static void Cuadrado_Valor(int numero)
+        {
+            numero = numero * numero;
+        }
+
+        static void Cuadrado_Referencia(ref int numero)
+        {
+            numero = numero * numero;
+        }
+    }
+}
