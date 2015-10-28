c8fdac4d0e72ff004554e96a77edd25017d91aed
Enrique Avila <senquevila@gmail.com>
2015-10-28 00:43:30 -0600

    Agregado Retiro.cs

---
+++ ATM/ATM/Retiro.cs
@@ -0,0 +1,40 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ATM{
+    /**
+     * 
+     */
+    public class Retiro : Transaccion {
+
+        /**
+         * 
+         */
+        public Retiro(string identificador, DateTime fecha, decimal monto) {
+            this.identificador = identificador;
+            this.fecha = fecha;
+            this.monto = monto;
+        }
+
+        /**
+         * 
+         */
+        private decimal monto;
+
+        /**
+         * 
+         */
+        public void Proceder() {
+            // TODO implement here
+        }
+
+        public override void Comisionar()
+        {
+            Console.WriteLine("Retiro: Transaccion si comisiona");
+        }
+
+    }
+}
